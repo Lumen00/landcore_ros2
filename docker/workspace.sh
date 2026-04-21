1794ec241dcc54ca0b60d5cda78bd5930c60ec9b
Damian Sue <damianbsue@gmail.com>
2026-04-21 02:56:42 +0000

    added -y to liblgpio-dev

--- docker/workspace.sh
+++ docker/workspace.sh
@@ -57,7 +57,7 @@ rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
 pip install PyQt6 
 pip install smbus3
 apt-get install libxcb-cursor0
-apt install liblgpio-dev
+apt install liblgpio-dev -y
 
 # Now apply all the fixes after dependencies are installed
 
