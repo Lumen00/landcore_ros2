0151602e6bf7830ab2486d618d37f7c7a789e1eb
Damian Sue <damianbsue@gmail.com>
2026-04-25 05:29:10 +0000

    gpiochip set to 0

--- dc_encoder_service/src/dc_encoder_node.cpp
+++ dc_encoder_service/src/dc_encoder_node.cpp
@@ -60,7 +60,7 @@ std::shared_ptr<dc_encoder_service::srv::MotorPI::Response> response){
 
 int openInputGPIO(int pin_no){
   // Open the GPIO
-  int h = lgGpiochipOpen(4);
+  int h = lgGpiochipOpen(0);
   if (h < 0){
     printf("ERROR: %s (%d)\n", lguErrorText(h), h);
   }
