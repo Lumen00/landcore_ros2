02081bb22e728db341583ce5db0dd3595c663fdf
Damian Sue <damianbsue@gmail.com>
2026-06-12 05:19:47 +0000

    removed min val for pwm

--- landwheeldrive/landwheeldrive/pid_tune.py
+++ landwheeldrive/landwheeldrive/pid_tune.py
@@ -70,7 +70,7 @@ class PID_Tuner(Node):
 		# self.speed_publisher.publish(msg)
 		# Begin recording encoder speeds.
 		start = time.perf_counter()
-		duration = 1
+		duration = 5
 		while (time.perf_counter() - start) < duration:
 			# Publish speed command.
 			if pwm != 0:
@@ -227,7 +227,7 @@ def main(args=None):
 
 	pid_pub = PID_Tuner()
 	pid_pub.encoder_client = PI_Client()
-	pid_pub.pid_tune(speed=float(0.5))
+	pid_pub.pid_tune(speed=float(0.25))
 	# pid_pub.pid_tune(speed=float(0), pwm=40)
 
 	pid_pub.destroy_node()
