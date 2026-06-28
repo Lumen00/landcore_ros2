f8e3381c39b146129704761a94524317c44df0c0
Damian Sue <damianbsue@gmail.com>
2026-06-28 06:40:54 +0000

    switch to using timer loop

--- landwheeldrive/landwheeldrive/cartesian_drive.py
+++ landwheeldrive/landwheeldrive/cartesian_drive.py
@@ -115,9 +115,8 @@ class Cartesian_Subscriber(Node):
         self.r = 0.04 # Radius of wheels
         self.current_msg = None
 
-        # Run PID_control() continuously. 
-        pid_thread = threading.Thread(target=self.PID_control, daemon=True)
-        pid_thread.start()
+        # Run PID_control() continuously on a timer.
+        self.timer = self.create_timer(0.02, self.PID_control)
         
     def listener_callback(self, msg):
         # The callback should exclusively be for updating the desired speed.
@@ -134,61 +133,59 @@ class Cartesian_Subscriber(Node):
 
     def PID_control(self):
         "Apply PID control to each motor value based on updating desired speed from subscriber."
-        "This should be run in a thread constantly which accesses outside values."
-        while(rclpy.ok()):
-            # Read current set speed into local variables.
-            lf_factor = (self.x - self.y - (self.lx + self.ly)*self.rot)/self.r
-            rf_factor = (self.x + self.y + (self.lx + self.ly)*self.rot)/self.r
-            lb_factor = (self.x + self.y - (self.lx + self.ly)*self.rot)/self.r
-            rb_factor = (self.x - self.y + (self.lx + self.ly)*self.rot)/self.r
-            wheels = [lf_factor, rf_factor, lb_factor, rb_factor]         
+        # Read current set speed into local variables.
+        lf_factor = (self.x - self.y - (self.lx + self.ly)*self.rot)/self.r
+        rf_factor = (self.x + self.y + (self.lx + self.ly)*self.rot)/self.r
+        lb_factor = (self.x + self.y - (self.lx + self.ly)*self.rot)/self.r
+        rb_factor = (self.x - self.y + (self.lx + self.ly)*self.rot)/self.r
+        wheels = [lf_factor, rf_factor, lb_factor, rb_factor]         
 
-            # Read current speeds from encoders.
-            response = self.pid.send_request(spd_in=wheels)
-            time_now = time.perf_counter()
-            response_time = time_now - self.last_response_time
-            self.last_response_time = time_now
+        # Read current speeds from encoders.
+        response = self.pid.send_request(spd_in=wheels)
+        time_now = time.perf_counter()
+        response_time = time_now - self.last_response_time
+        self.last_response_time = time_now
 
-            # Calculate errors
-            # P error
-            errors = [
-                lf_factor - response.speed_front_left,
-                rf_factor - response.speed_front_right,
-                lb_factor - response.speed_back_left,
-                rb_factor - response.speed_back_right,
+        # Calculate errors
+        # P error
+        errors = [
+            lf_factor - response.speed_front_left,
+            rf_factor - response.speed_front_right,
+            lb_factor - response.speed_back_left,
+            rb_factor - response.speed_back_right,
+        ]
+        # I & D error
+        if self.old_msg == self.current_msg:
+            self.D_error = [
+                (errors[id] - last_error)/response_time for id, last_error in enumerate(self.prev_error)
             ]
-            # I & D error
-            if self.old_msg == self.current_msg:
-                self.D_error = [
-                    (errors[id] - last_error)/response_time for id, last_error in enumerate(self.prev_error)
-                ]
-                self.I_error = [
-                    add_error + errors[id]*response_time for id, add_error in enumerate(self.I_error)
-                ]
-            else: 
-                self.D_error = [0,0,0,0]
-                self.I_error = [0,0,0,0]
-                self.prev_error = [0,0,0,0]
+            self.I_error = [
+                add_error + errors[id]*response_time for id, add_error in enumerate(self.I_error)
+            ]
+        else: 
+            self.D_error = [0,0,0,0]
+            self.I_error = [0,0,0,0]
+            self.prev_error = [0,0,0,0]
                 
-            # Apply Feed Forward + PID control to calculate PWM.
-            pwm = [
-                max(0, min(255, lf_factor/0.3 + self.Kp[0]*errors[0] + self.Ki[0]*self.I_error[0] + self.Kd[0]*self.D_error[0])), # Left Front    
-                max(0, min(255, rf_factor/0.3 + self.Kp[1]*errors[1] + self.Ki[1]*self.I_error[1] + self.Kd[1]*self.D_error[1])), # Right Front   
-                max(0, min(255, lb_factor/0.3 + self.Kp[2]*errors[2] + self.Ki[2]*self.I_error[2] + self.Kd[2]*self.D_error[2])), # Left Back     
-                max(0, min(255, rb_factor/0.3 + self.Kp[3]*errors[3] + self.Ki[3]*self.I_error[3] + self.Kd[3]*self.D_error[3])) # Right Back    
-            ] 
+        # Apply Feed Forward + PID control to calculate PWM.
+        pwm = [
+            max(0, min(255, lf_factor/0.3 + self.Kp[0]*errors[0] + self.Ki[0]*self.I_error[0] + self.Kd[0]*self.D_error[0])), # Left Front    
+            max(0, min(255, rf_factor/0.3 + self.Kp[1]*errors[1] + self.Ki[1]*self.I_error[1] + self.Kd[1]*self.D_error[1])), # Right Front   
+            max(0, min(255, lb_factor/0.3 + self.Kp[2]*errors[2] + self.Ki[2]*self.I_error[2] + self.Kd[2]*self.D_error[2])), # Left Back     
+            max(0, min(255, rb_factor/0.3 + self.Kp[3]*errors[3] + self.Ki[3]*self.I_error[3] + self.Kd[3]*self.D_error[3])) # Right Back    
+        ] 
 
-            # Apply transformation to account for wheels spinning the other way.
-            if self.current_msg:
-                self.get_logger().info(f'heard {self.current_msg.data} transformed to {pwm}')
+        # Apply transformation to account for wheels spinning the other way.
+        if self.current_msg:
+            self.get_logger().info(f'heard {self.current_msg.data} transformed to {pwm}')
 
-            # Command motors to run at PWMs
-            self.run_motor(left_front, int(pwm[0]))
-            self.run_motor(right_front, int(pwm[1]))
-            self.run_motor(left_back, -int(pwm[2]))
-            self.run_motor(right_back, -int(pwm[3]))
+        # Command motors to run at PWMs
+        self.run_motor(left_front, int(pwm[0]))
+        self.run_motor(right_front, int(pwm[1]))
+        self.run_motor(left_back, -int(pwm[2]))
+        self.run_motor(right_back, -int(pwm[3]))
 
-            self.prev_error = errors
+        self.prev_error = errors
 
     def run_motor(self, motor:DC_Motor, value:int):
         # motor.mh.setSpeed(255)
