0ba4adce67862dce311911e0af68ff9733401a81
Damian Sue <damianbsue@gmail.com>
2026-08-04 03:04:12 +0000

    test for including c1 launch in land_description

--- land_description/launch/land_bringup.launch.py
+++ land_description/launch/land_bringup.launch.py
@@ -1,12 +1,22 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 import xacro
 import os
 
+# Imports for bringing in launch files from other packages.
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 def generate_launch_description():
     urdf_path = os.path.join(get_package_share_directory('land_description'), 'urdf', 'land_description.urdf.xacro')
     controllers_yaml = os.path.join(get_package_share_directory('land_description'), 'config', 'controllers.yaml')
+
+    # Import LiDAR launch for C1.
+    c1_launch_dir = PathJoinSubstitution([FindPackageShare('sllidar_ros2'), 'launch'])
+    c1_launch = IncludeLaunchDescription(
+        PathJoinSubstitution([c1_launch_dir, 'sllidar_c1_launch.py'])
+    )
 
     # robot_description = {'robot_description': open(urdf_path).read()}  # or xacro.process_file()
     robot_description = {'robot_description': xacro.process_file(urdf_path).toxml()}
@@ -34,4 +44,11 @@ def generate_launch_description():
         arguments=['mecanum_drive_controller']
     )
 
-    return LaunchDescription([robot_state_publisher, controller_manager, spawn_jsb, spawn_mecanum])
+    # Include the C1 LiDAR launch file. 
+
+
+    return LaunchDescription([robot_state_publisher, 
+        controller_manager, 
+        spawn_jsb, 
+        spawn_mecanum,
+        c1_launch])
