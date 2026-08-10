939791a737c9c5dd651d2bee6901b4dc023ef798
Damian Sue <damianbsue@gmail.com>
2026-08-10 11:10:51 +0000

    swap to opencv for images

--- land_camera/land_camera/camera_node.py
+++ land_camera/land_camera/camera_node.py
@@ -1,43 +1,60 @@
 import rclpy
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 import threading
 from picamera2 import Picamera2
 from cv_bridge import CvBridge # converts between opencv and ros images
+import cv2
+import numpy as np
 
 class CameraImagePublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Image, '/camera/image_raw', qos_profile_sensor_data)
+        self.publisher_ = self.create_publisher(CompressedImage, '/camera/image_raw', qos_profile_sensor_data)
 
         # Init the bridge
-        self.bridge = CvBridge()
+        # self.bridge = CvBridge()
 
-        # Init the camera.
-        self.picam = Picamera2()
-        config = self.picam.create_video_configuration(
-            main={"size": (640, 480), "format": "RGB888"}
-        )
-        self.picam.configure(config)
-        self.picam.start()
+        self.timer = self.create_timer(0.05, self.camera_publish_loop)
 
-        # self.timer = self.create_timer(0.033, self.camera_publish_loop)
+        self.cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        self.get_logger().info('Publishing compressed image...')
 
-        self.stream_thread = threading.Thread(target=self.camera_publish_loop, daemon=True)
-        self.stream_thread.start()
+        # # Init the camera.
+        # self.picam = Picamera2()
+        # config = self.picam.create_video_configuration(
+        #     main={"size": (640, 480), "format": "RGB888"}
+        # )
+        # self.picam.configure(config)
+        # self.picam.start()
+
+        # # self.timer = self.create_timer(0.033, self.camera_publish_loop)
+
+        # self.stream_thread = threading.Thread(target=self.camera_publish_loop, daemon=True)
+        # self.stream_thread.start()
 
     def camera_publish_loop(self):
-        # self.get_logger().info('Camera starting to publish.')
-        while rclpy.ok():
-            # try:
-                # Take a frame
-            frame = self.picam.capture_array().copy()
-            msg = self.bridge.cv2_to_imgmsg(frame, encoding='rgb8')
+        ret, frame = self.cap.read()
+        if ret:
+            msg = CompressedImage()
             msg.header.stamp = self.get_clock().now().to_msg()
-            msg.header.frame_id = 'camera_link'
+            msg.format = "jpeg"
+            msg.data = np.array(cv2.imencode('.jpg', frame)[1]).tobytes()
             self.publisher_.publish(msg)
+
+        # self.get_logger().info('Camera starting to publish.')
+        # while rclpy.ok():
+        #     # try:
+        #         # Take a frame
+        #     frame = self.picam.capture_array().copy()
+        #     msg = self.bridge.cv2_to_imgmsg(frame, encoding='rgb8')
+        #     msg.header.stamp = self.get_clock().now().to_msg()
+        #     msg.header.frame_id = 'camera_link'
+        #     self.publisher_.publish(msg)
         # except Exception as e:
         # self.get_logger().error(f'Publish loop failed: {e}')
 
@@ -47,11 +64,11 @@ def main(args=None):
 
     camera_publisher = CameraImagePublisher()
 
-    # rclpy.spin(camera_publisher)
+    rclpy.spin(camera_publisher)
 
-    camera_publisher.stream_thread.join()
+    # camera_publisher.stream_thread.join()
 
-    camera_publisher.picam.stop()
+    # camera_publisher.picam.stop()
     camera_publisher.destroy_node()
     rclpy.shutdown()
 
