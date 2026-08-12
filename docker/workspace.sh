9e7bf0cb95b1c1565ce6f39fce88b5309cff406e
Damian Sue <damianbsue@gmail.com>
2026-08-12 03:09:48 +0000

    fix for ensurepip error

--- docker/workspace.sh
+++ docker/workspace.sh
@@ -18,7 +18,7 @@ rosdep update
 rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
 
 # Install pip dependencies.
-python3 -m venv my
+python3 -m venv my --without-pip --system-site-packages #Bypass ensure-pip error on ubuntu
 pip install PyQt6 smbus3 matplotlib picamera2
 apt-get install libxcb-cursor0
 apt install liblgpio-dev \
